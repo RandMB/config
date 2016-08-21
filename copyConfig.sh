ca2a08cd030b585035002a14d0702a14b7995095
Mantas Bartkus <mantasb74@gmail.com>
2016-08-22 01:08:33 +0300

    a

---
+++ copyConfig.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+cp -R $HOME/.config/i3/ $HOME/Git/config/.config/
+cp -R $HOME/.config/i3status/ $HOME/Git/config/.config/
+cp $HOME/.config/compton.conf $HOME/Git/config/.config
+#cp /usr/local/bin/touchpadToggle.sh $HOME/Git/config/
+cp $HOME/Scripts/copyConfig.sh $HOME/Git/config/
