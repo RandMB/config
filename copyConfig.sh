11af2b39768c4be1db0ad9a1f8f7683f6e60ebd5
Mantas Bartkus <mantasb74@gmail.com>
2016-08-22 01:17:44 +0300

    a

--- copyConfig.sh
+++ copyConfig.sh
@@ -5,3 +5,8 @@ cp -R $HOME/.config/i3status/ $HOME/Git/config/.config/
 cp $HOME/.config/compton.conf $HOME/Git/config/.config
 #cp /usr/local/bin/touchpadToggle.sh $HOME/Git/config/
 cp $HOME/Scripts/copyConfig.sh $HOME/Git/config/
+cp -R $HOME/.config/fontconfig/ $HOME/Git/config/.config/
+cp -R $HOME/.config/gtk-3.0/ $HOME/Git/config/.config/
+cp $HOME/.gtkrc-2.0 $HOME/Git/config/
+cp $HOME/.zshrc $HOME/Git/config/
+
