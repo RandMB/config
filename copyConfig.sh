8cc3fb28bd5fcb2f54fb215e993fc8529edfeb01
Mantas Bartkus <mantasb74@gmail.com>
2016-08-22 01:22:01 +0300

    a

--- copyConfig.sh
+++ copyConfig.sh
@@ -2,7 +2,8 @@
 
 cp -R $HOME/.config/i3/ $HOME/Git/config/.config/
 cp -R $HOME/.config/i3status/ $HOME/Git/config/.config/
-cp $HOME/.config/compton.conf $HOME/Git/config/.config
+cp $HOME/.config/compton.conf $HOME/Git/config/.config/
+cp $HOME/.config/Trolltech.conf $HOME/Git/config/.config/
 #cp /usr/local/bin/touchpadToggle.sh $HOME/Git/config/
 cp $HOME/Scripts/copyConfig.sh $HOME/Git/config/
 cp -R $HOME/.config/fontconfig/ $HOME/Git/config/.config/
