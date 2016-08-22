c934e8db8c59bf32f25fd4e0fd1e45bd425fc5ef
Mantas Bartkus <mantasb74@gmail.com>
2016-08-22 23:31:53 +0300

    a

--- copyConfig.sh
+++ copyConfig.sh
@@ -9,6 +9,7 @@ cp $HOME/Scripts/copyConfig.sh $HOME/Git/config/
 cp -R $HOME/.config/fontconfig/ $HOME/Git/config/.config/
 cp -R $HOME/.config/gtk-3.0/ $HOME/Git/config/.config/
 cp -R $HOME/.config/qt5ct/ $HOME/Git/config/.config/
+cp -R $HOME/.config/mpv/ $HOME/Git/config/.config/
 cp $HOME/.gtkrc-2.0 $HOME/Git/config/
 cp $HOME/.zshrc $HOME/Git/config/
 
