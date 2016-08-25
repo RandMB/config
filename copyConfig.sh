0adcf6521f210a03f734292fd402769bdb203ccd
Mantas Bartkus <mantasb74@gmail.com>
2016-08-25 14:36:56 +0300

    add mpd and ncmpcpp

--- copyConfig.sh
+++ copyConfig.sh
@@ -1,15 +1,18 @@
 #! /bin/sh
 
-cp -R $HOME/.config/i3/ $HOME/Git/config/.config/
-cp -R $HOME/.config/i3status/ $HOME/Git/config/.config/
 cp $HOME/.config/compton.conf $HOME/Git/config/.config/
 cp $HOME/.config/Trolltech.conf $HOME/Git/config/.config/
+cp $HOME/.config/mpd/mpd.conf $HOME/Git/config/.config/mpd
+cp $HOME/.config/ncmpcpp/config $HOME/Git/config/.config/ncmpcpp/
 #cp /usr/local/bin/touchpadToggle.sh $HOME/Git/config/
-cp $HOME/Scripts/copyConfig.sh $HOME/Git/config/
+cp -R $HOME/.config/i3/ $HOME/Git/config/.config/
+cp -R $HOME/.config/i3status/ $HOME/Git/config/.config/
 cp -R $HOME/.config/fontconfig/ $HOME/Git/config/.config/
 cp -R $HOME/.config/gtk-3.0/ $HOME/Git/config/.config/
 cp -R $HOME/.config/qt5ct/ $HOME/Git/config/.config/
 cp -R $HOME/.config/mpv/ $HOME/Git/config/.config/
+cp -R $HOME/.config/mpd/playlists/ $HOME/Git/config/.config/mpd/
 cp $HOME/.gtkrc-2.0 $HOME/Git/config/
 cp $HOME/.zshrc $HOME/Git/config/
 cp /opt/vivaldi-snapshot/resources/vivaldi/style/custom.css $HOME/Git/config/vivaldi-custom.css
+cp $HOME/Scripts/copyConfig.sh $HOME/Git/config/
